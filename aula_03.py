d41018b25e36539e79042541d1835d0da7e4e3f2
GUSTAVO ANTONIO <gustavo.antonio@senacsp.edu.br>
2026-08-18 17:06:20 -0300

    Continuação da aula

---
+++ aula_03.py
@@ -0,0 +1,25 @@
+#Operador de comparação
+a = 10
+b = 5
+
+print(a > b) # maior
+print(a < b) # menor
+print(a == b) #igual
+print(a != b) #diferente
+print(a >= b) #maior ou igural
+print(a <= b) #menor ou igual
+
+
+#Operadores lógicos
+numero = 10
+print('Operadores Lõgicos')
+#No AND o resultado é true se os dois lados forem true
+print(numero > 5 and numero < 15)
+
+# No OR o resultado é true se somente um dos lados for true
+print(numero < 5 or numero == 10 )
+
+#Not pega o bool original e nega ele
+print(not( numero > 5))
+
+
